904e1ada4de845b962ef233ef1d093481b2fa585
Le_Bugurt <shentsevartem@yandex.ru>
2024-10-01 00:08:55 +0300

    Initial commit

---
+++ src/main/java/bugurt/vacancy/vacancyprocessing/VacancyProcessingApplication.java
@@ -0,0 +1,13 @@
+package bugurt.vacancy.vacancyprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacancyProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacancyProcessingApplication.class, args);
+	}
+
+}
